c14e8bcf1423a5c14ec3b7640c111f81d49ff340
VIPABH <a891983.2@gmail.com>
2025-07-04 00:47:16 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,7 +1,7 @@
 from ABH import ABH, events
 import json, pytz, asyncio, os, sys
 from datetime import datetime
-from code import *
+from وعد import *
 async def run_cmd(command: str):
     process = await asyncio.create_subprocess_shell(
         command,
@@ -18,9 +18,57 @@ async def update_repo(event):
         os.execv(sys.executable, [sys.executable, "p.py"])
     else:
         await event.reply(f" حدث خطأ أثناء التحديث:\n\n{stderr}")
-def main():
-    print("config is starting...")
-    ABH.start(bot_token=bot_token)
-    ABH.run_until_disconnected()
+
+@ABH.on(events.NewMessage(pattern="^اطفاء$", from_users=[1910015590]))
+async def shutdown(event):
+    me = await ABH.get_me()
+    id = me.id
+    if id == 1910015590:
+        return
+    await event.reply("🔴 جارٍ إيقاف اليوزربوت ...")
+    await asyncio.sleep(1)
+    await ABH.disconnect()
+    await ABH.disconnect()
+    sys.exit(0)
+@ABH.on(events.NewMessage(pattern="^رست$", from_users=[1910015590]))
+async def resetbot(event):
+    id = await event.get_reply_message()
+    if id and id.id == 1910015590:
+        return
+    await asyncio.sleep(1)
+    await restart_bot(event)
+@ABH.on(events.NewMessage(pattern="^.حدث$", from_users=[1910015590]))
+async def resetbot(event):
+    id = await event.get_reply_message()
+    if id and id.id == 1910015590:
+        return
+    await asyncio.sleep(1)
+    await update_repo(event)
+@ABH.on(events.NewMessage(pattern="^اعادة تشغيل$", outgoing=True))
+async def restart_bot(event):
+    await event.respond("♻️ جارٍ إعادة تشغيل اليوزربوت ...")
+    await asyncio.sleep(1)
+    os.execv(sys.executable, [sys.executable, "run.py"])
+async def run_cmd(command: str):
+    process = await asyncio.create_subprocess_shell(
+        command,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return stdout.decode().strip(), stderr.decode().strip(), process.returncode
+@ABH.on(events.NewMessage(pattern="^.تحديث$", outgoing=True))
+async def update_repo(event):
+    msg = await event.respond(" جاري جلب آخر التحديثات من الريبو عبر...")
+    stdout, stderr, code = await run_cmd("git pull")
+    if code == 0:
+        await msg.edit(f" تحديث السورس بنجاح")
+        os.execv(sys.executable, [sys.executable, "run.py"])
+    else:
+        await msg.edit(f" حدث خطأ أثناء التحديث:\n\n{stderr}")
+async def main():
+    await ABH.start()
+    await ABH.run_until_disconnected()
 if __name__ == "__main__":
-    main()
+    import asyncio
+    asyncio.run(main())
