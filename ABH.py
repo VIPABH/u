d0a2851a9a00589952b849fc2087a1ef1479c9be
VIPABH <a891983.2@gmail.com>
2025-09-20 00:39:22 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -1,17 +1,30 @@
-from telethon import events ,TelegramClient
+from telethon import events, TelegramClient
 import os
+wfffp = 1910015590
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("bot_token")
+bot = TelegramClient("code", api_id, api_hash).start(bot_token=bot_token)
 api_id1 = int(os.getenv("API_ID1"))
 api_hash1 = os.getenv("API_HASH1")
-ABH1 = TelegramClient("code1", api_id1, api_hash1)
+ABH1 = TelegramClient("code1", api_id1, api_hash1).start()
 api_id2 = int(os.getenv("API_ID2"))
 api_hash2 = os.getenv("API_HASH2")
-ABH2 = TelegramClient("code2", api_id2, api_hash2)
+ABH2 = TelegramClient("code2", api_id2, api_hash2).start()
 api_id3 = int(os.getenv("API_ID3"))
 api_hash3 = os.getenv("API_HASH3")
-ABH3 = TelegramClient("code3", api_id3, api_hash3)
+ABH3 = TelegramClient("code3", api_id3, api_hash3).start()
 api_id4 = int(os.getenv("API_ID4"))
 api_hash4 = os.getenv("API_HASH4")
-ABH4 = TelegramClient("code4", api_id4, api_hash4)
+ABH4 = TelegramClient("code4", api_id4, api_hash4).start()
 api_id5 = int(os.getenv("API_ID5"))
 api_hash5 = os.getenv("API_HASH5")
-ABH5 = TelegramClient("code5", api_id5, api_hash5)
+ABH5 = TelegramClient("code5", api_id5, api_hash5).start()
+ABHS = [ABH1, ABH2, ABH3, ABH4, ABH5]
+@bot.on(events.NewMessage(pattern='^ارسل$', from_users=wfffp))
+async def s(e):
+    if e.is_private:
+        for ABH in ABHS:
+            await ABH.send_message(wfffp, 'ها')
+print("✅ البوت والحسابات الإضافية اشتغلوا")
+bot.run_until_disconnected()
