46d7c8c66b0c3eebd9bacd4ddfccbb81a7aa37d6
VIPABH <a891983.2@gmail.com>
2025-09-20 00:58:05 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -12,16 +12,17 @@ ABH1 = TelegramClient("code1", api_id1, api_hash1).start()
 api_id2 = int(os.getenv("API_ID2"))
 api_hash2 = os.getenv("API_HASH2")
 ABH2 = TelegramClient("code2", api_id2, api_hash2).start()
-api_id3 = int(os.getenv("API_ID3"))
-api_hash3 = os.getenv("API_HASH3")
-ABH3 = TelegramClient("code3", api_id3, api_hash3).start()
+# api_id3 = int(os.getenv("API_ID3"))
+# api_hash3 = os.getenv("API_HASH3")
+# ABH3 = TelegramClient("code3", api_id3, api_hash3).start()
 api_id4 = int(os.getenv("API_ID4"))
 api_hash4 = os.getenv("API_HASH4")
 ABH4 = TelegramClient("code4", api_id4, api_hash4).start()
 api_id5 = int(os.getenv("API_ID5"))
 api_hash5 = os.getenv("API_HASH5")
 ABH5 = TelegramClient("code5", api_id5, api_hash5).start()
-ABHS = [ABH1, ABH2, ABH3, ABH4, ABH5]
+# ABHS = [ABH1, ABH2, ABH3, ABH4, ABH5]
+ABHS = [ABH1, ABH2, ABH4, ABH5]
 @bot.on(events.NewMessage(pattern='^ارسل$', from_users=wfffp))
 async def s(e):
     if e.is_private:
