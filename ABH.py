885bfa84ec18f22e4a651aa2c1bf561a4af24558
VIPABH <a891983.2@gmail.com>
2025-09-20 14:56:46 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -21,7 +21,10 @@ api_id5 = int(os.getenv("API_ID5"))
 api_hash5 = os.getenv("API_HASH5")
 ABH5 = TelegramClient("code5", api_id5, api_hash5).start()
 ABHS = [ABH1, ABH2, ABH3, ABH4, ABH5]
-@bot.on(events.NewMessage(pattern=r'^ارسل(?: (\d+))?$', from_users=wfffp))
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.errors import UserAlreadyParticipantError
+@bot.on(events.NewMessage(pattern=r'^ارسل(?: (\S+))?$', from_users=wfffp))
 async def s(e):
     reply = await e.get_reply_message()
     if not reply:
@@ -29,7 +32,24 @@ async def s(e):
     num = e.pattern_match.group(1) or str(wfffp)
     for ABH in ABHS:
         try:
-            entity = await ABH.get_entity(int(num)) if num.isdigit() else await ABH.get_entity(num)
+            entity = None
+            if num.isdigit():
+                entity = await ABH.get_entity(int(num))
+            else:
+                try:
+                    entity = await ABH.get_entity(num)
+                except ValueError:
+                    if "t.me/+" in num or "joinchat" in num:
+                        invite = num.split("/")[-1].replace("+", "")
+                        try:
+                            entity = await ABH(ImportChatInviteRequest(invite))
+                        except UserAlreadyParticipantError:
+                            entity = await ABH.get_entity(num)
+            if entity and hasattr(entity, "id"):
+                try:
+                    await ABH(JoinChannelRequest(entity))
+                except UserAlreadyParticipantError:
+                    pass
             if reply.text and not reply.media:
                 await ABH.send_message(entity, reply.text)
             elif reply.media:
