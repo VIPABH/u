9c19c71ed5e5d87ce08fc72772a4022e5d9c8d6c
VIPABH <a891983.2@gmail.com>
2025-10-01 00:09:20 +0300

    Update وعد.py

--- وعد.py
+++ وعد.py
@@ -2,32 +2,6 @@ from telethon import events
 import asyncio, re
 from ABH import *
 target_user_id = 1421907917
-@bot.on(events.NewMessage(pattern='!تجربة', from_users=[wfffp]))
-async def test(e):
-    for ABH in ABHS:
-        await ABH.send_message(wfffp, 'نعم')
-@bot.on(events.NewMessage(pattern=r"^.?كلمات (\d+)\s+(\d+)$", from_users=[1910015590, 201728276]))
-async def words(event):
-    num = int(event.pattern_match.group(1)) or 1
-    time = int(event.pattern_match.group(2)) or 1
-    for ABH in ABHS:
-        for i in range(num):
-            async with ABH.conversation(event.chat_id, timeout=10) as conv:
-                await conv.send_message("كلمات")
-                try:
-                    while True:
-                        msg = await conv.get_response()
-                        if msg.sender_id != target_user_id:
-                            continue
-                        text = msg.raw_text.strip()
-                        match = re.search(r"\(\s*(.+?)\s*\)", text)
-                        if match:
-                            text = match.group(1)
-                            await asyncio.sleep(time)
-                            await conv.send_message(text)
-                        break
-                except asyncio.TimeoutError:
-                    return
 @bot.on(events.NewMessage(pattern=r"^.?تركيب (\d+)$", from_users=[1910015590, 201728276]))
 async def unspilt(event):
     num = int(event.pattern_match.group(1)) or 1
