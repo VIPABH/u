1766dc6f1b0ad18d2f939a3c1ac1be4e1f6f9f1a
VIPABH <a891983.2@gmail.com>
2025-10-01 00:09:30 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -32,6 +32,32 @@ api_id5 = int(os.getenv("API_ID5"))
 api_hash5 = os.getenv("API_HASH5")
 ABH5 = TelegramClient("code5", api_id5, api_hash5).start()
 ABHS = [ABH1, ABH2, ABH3, ABH4, ABH5]
+@bot.on(events.NewMessage(pattern='تجربة', from_users=[wfffp]))
+async def test(e):
+    for ABH in ABHS:
+        await ABH.send_message(wfffp, 'نعم')
+@bot.on(events.NewMessage(pattern=r"^.?كلمات (\d+)\s+(\d+)$", from_users=[1910015590, 201728276]))
+async def words(event):
+    num = int(event.pattern_match.group(1)) or 1
+    time = int(event.pattern_match.group(2)) or 1
+    for ABH in ABHS:
+        for i in range(num):
+            async with ABH.conversation(event.chat_id, timeout=10) as conv:
+                await conv.send_message("كلمات")
+                try:
+                    while True:
+                        msg = await conv.get_response()
+                        if msg.sender_id != target_user_id:
+                            continue
+                        text = msg.raw_text.strip()
+                        match = re.search(r"\(\s*(.+?)\s*\)", text)
+                        if match:
+                            text = match.group(1)
+                            await asyncio.sleep(time)
+                            await conv.send_message(text)
+                        break
+                except asyncio.TimeoutError:
+                    return
 @bot.on(events.NewMessage(pattern=r'^ارسل(?: (\S+))?$', from_users=wfffp))
 async def s(e):
     reply = await e.get_reply_message()
