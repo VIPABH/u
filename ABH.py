4e7135ff02e1e42d8c7158c47d5fa5b420620aed
VIPABH <a891983.2@gmail.com>
2025-10-15 15:53:09 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -1,7 +1,9 @@
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
-from telethon.tl.functions.messages import ExportChatInviteRequest
 from telethon.tl.functions.messages import SendReactionRequest
+from telethon.tl.types import ReactionEmoji
+from telethon.tl.functions.channels import EditAdminRequest
+from telethon.tl.types import ChatAdminRights, Channel
 from telethon.tl.types import ReactionEmoji
 from telethon.errors import UserAlreadyParticipantError
 from telethon.errors import ChatAdminRequiredError
