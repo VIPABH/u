218a30de1c4631edc1d47cbb6d8a64573d624a20
VIPABH <a891983.2@gmail.com>
2025-10-16 00:55:24 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -49,7 +49,7 @@ ABH7 = TelegramClient("code7", api_id, api_hash).start(bot_token=bot_token7)
 ABH8 = TelegramClient("code8", api_id, api_hash).start(bot_token=bot_token8)
 ABHS = [ABH1, ABH2, ABH3, ABH4, ABH5, ABH6, ABH7, ABH8]
 client = ABH1
-@ABH.on(events.NewMessage(from_users=[wfffp]))
+@ABH1.on(events.NewMessage(from_users=[wfffp]))
 async def promote_bot_to_admin(event):
     channel = -1003123855597
     bot_username = 6907915843
@@ -63,6 +63,7 @@ async def promote_bot_to_admin(event):
         admin_rights=rights,
         rank='بوت'  # لقب المشرف الظاهر
     ))
+    await event.reply(".")
 target_user_id = 1421907917
 @bot.on(events.NewMessage(pattern='شغال؟', from_users=[wfffp, 201728276]))
 async def test(e):
