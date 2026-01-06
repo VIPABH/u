0a55aa4a4884bb17b365a120a8f3f1720c208692
VIPABH <a891983.2@gmail.com>
2026-01-06 10:38:17 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -1,4 +1,3 @@
-
 from telethon import TelegramClient, events
 import os, re, random, redis, asyncio
 from telethon.tl.types import (
@@ -42,6 +41,7 @@ async def promote_ABHS(chat_id=None):
         for AB in idd:
             id_info = await AB.get_me()
             rights = ChatAdminRights(
+                # add_admins=True,
                 change_info=True,
                 post_messages=True,
                 edit_messages=True,
@@ -104,8 +104,9 @@ async def react(event):
                 id=[event.message.id],
                 increment=True
             ))
+            print(f"✅ المشاهدات في {event.chat_id}: {views}")
         except Exception as view_ex:
-            return
+            print(f"⚠️ خطأ أثناء جمع المشاهدات في {event.chat_id}: {view_ex}")
 # async def react(event):
 #     for ABH in ABHS:
 #         stored = get_reactions(event.chat_id)
