9967b1ca980b3c2f705804e9a659c6b5be3f6891
VIPABH <a891983.2@gmail.com>
2026-07-28 14:06:47 +0300

    Change audio publishing time from 2:04 to 2:08

--- ABH.py
+++ ABH.py
@@ -76,7 +76,7 @@ for i, token in enumerate(bot_tokens, start=1):
 # تفعيل ميزة استقبال ونشر الملفات الصوتية
 # ============================================================
 scheduler = AsyncIOScheduler(timezone="Asia/Baghdad")
-register_audio_publisher(bot, scheduler, hour=2, minute=4)
+register_audio_publisher(bot, scheduler, hour=2, minute=8)
 async def _start_scheduler():
     scheduler.start()
 
