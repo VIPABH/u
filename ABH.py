c8d7c82077652c5bea62d46b6c37165c6b66c969
VIPABH <a891983.2@gmail.com>
2026-08-14 11:16:43 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -17,7 +17,8 @@ api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("bot_token")
 bot = TelegramClient("botcode", api_id, api_hash).start(bot_token=bot_token)
-clients = {}
+mainABH = TelegramClient(wfffp, int(api_id), api_hash).start()
+clients = {mainABH}
 sessions = ["code1", "code2", "code3", "code4", "code5", "code6",
             "code7", "code8", "code9", "code10", "code11", "code12", "code13", "code14", 'code15']
 for i, session in enumerate(sessions, start=1):
@@ -29,7 +30,6 @@ for i, session in enumerate(sessions, start=1):
         print(f"{session} is working!")
     else:
         print(f"Skipping {session} due to missing environment variables.")
-mainABH = clients.get("wfffp")
 ABH1 = clients.get("code1")
 ABH2 = clients.get("code2")
 ABH3 = clients.get("code3")
