ed5233c57d98848fb14872326ecbab1c842679b1
VIPABH <a891983.2@gmail.com>
2026-08-14 11:11:54 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -18,7 +18,7 @@ api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("bot_token")
 bot = TelegramClient("botcode", api_id, api_hash).start(bot_token=bot_token)
 clients = {}
-sessions = ["wfffp", "code1", "code2", "code3", "code4", "code5", "code6",
+sessions = ["code1", "code2", "code3", "code4", "code5", "code6",
             "code7", "code8", "code9", "code10", "code11", "code12", "code13", "code14", 'code15']
 for i, session in enumerate(sessions, start=1):
     api_id_i = os.getenv(f"API_ID{i}")
